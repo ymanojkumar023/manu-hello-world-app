dd0a96889936f4bd486149bb334d03adf491c901
kumar_mano <ymanojkumar023@gmail.com>
2019-10-16 18:29:58 +0530

    testing with reder html

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ from math import ceil
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+'''
 @app.route("/")
 def hello():
     return "Hello World!"
+'''
