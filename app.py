c0b7525ef40f6bb130f66847d682f2556cb947b0
kumar_mano <ymanojkumar023@gmail.com>
2019-10-16 18:45:21 +0530

    testing with reder html-2

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ import numpy as np
 
 import time
 from math import ceil
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
-from flask import Flask
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,4 +22,11 @@ def index():
 @app.route("/")
 def hello():
     return "Hello World!"
+'''
+'''
+if __name__ == '__main__':
+    # Serve the app with gevent
+    # http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server = WSGIServer(app)
+    http_server.serve_forever()
 '''
